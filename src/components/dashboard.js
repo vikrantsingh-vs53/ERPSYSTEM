74b6a734b4e613a9e22c95f00bc0d7edac9b7853
VIKRANT SINGH <VIKRANTSINGH.VS53@GMAIL.COM>
2024-03-13 11:55:46 +0530

    updated site

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,3 +1,26 @@
-export default function Dashboard(){
-    return <h1>Dashboard</h1>
-}
+import React from 'react';
+import Orders from './orders';
+import Product from './product';
+import Calendar from './Calendar';
+import './dashboard.css';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+      <h1>Dashboard</h1>
+      <div className="dashboard-content">
+        <div className="tiles-section">
+          <Orders />
+          <Product />
+        </div>
+
+        <div className="calendar-section">
+          <Calendar />
+        </div>
+      </div>
+
+    </div>
+    );
+    };
+
+export default Dashboard;
